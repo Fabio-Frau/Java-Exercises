39e2787b87a7abb51a41f5a15e7ca9cd6d979b2f
Fabio Frau <fbfr91@yahoo.it>
2023-09-25 22:31:26 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   Assigned/Exercise 1-2/Assignment.txt
    	new file:   Assigned/Exercise 1-2/Solution/.gitignore
    	new file:   Assigned/Exercise 1-2/Solution/.idea/.gitignore
    	new file:   Assigned/Exercise 1-2/Solution/.idea/misc.xml
    	new file:   Assigned/Exercise 1-2/Solution/.idea/modules.xml
    	new file:   Assigned/Exercise 1-2/Solution/.idea/vcs.xml
    	new file:   Assigned/Exercise 1-2/Solution/Solution.iml
    	new file:   Assigned/Exercise 1-2/Solution/src/Start.java
    
    Description: second assigned exercise solved

---
+++ Assigned/Exercise 1-2/Solution/src/Start.java
@@ -0,0 +1,9 @@
+public class Start {
+    public static void main(String[] args) {
+        char firstChar = 'f';
+        char secondChar = 's';
+
+        String result = "" + firstChar + secondChar;
+        System.out.println(result);
+    }
+}
