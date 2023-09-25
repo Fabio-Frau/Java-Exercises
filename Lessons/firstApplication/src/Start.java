5cd861156b9e02d16cee18d3dff23895f9897535
Fabio Frau <fbfr91@yahoo.it>
2023-09-25 14:53:09 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   Lessons/firstApplication/.gitignore
    	new file:   Lessons/firstApplication/.idea/.gitignore
    	new file:   Lessons/firstApplication/.idea/misc.xml
    	new file:   Lessons/firstApplication/.idea/modules.xml
    	new file:   Lessons/firstApplication/.idea/vcs.xml
    	new file:   Lessons/firstApplication/firstApplication.iml
    	new file:   Lessons/firstApplication/src/Start.java
    
    Description: I added my folder structure and my first exercise

---
+++ Lessons/firstApplication/src/Start.java
@@ -0,0 +1,11 @@
+public class Start {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 25;
+        int z = x + y;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("Sum of x + y = " + z);
+    }
+}
