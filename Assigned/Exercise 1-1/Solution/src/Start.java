4d6ed18e2939e4e17a56830a7e2e28992a09c6db
Fabio Frau <fbfr91@yahoo.it>
2023-09-25 22:24:28 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   Assigned/Exercise 1-1/Assignment.txt
    	new file:   Assigned/Exercise 1-1/Solution/.gitignore
    	new file:   Assigned/Exercise 1-1/Solution/.idea/.gitignore
    	new file:   Assigned/Exercise 1-1/Solution/.idea/misc.xml
    	new file:   Assigned/Exercise 1-1/Solution/.idea/modules.xml
    	new file:   Assigned/Exercise 1-1/Solution/.idea/vcs.xml
    	new file:   Assigned/Exercise 1-1/Solution/Solution.iml
    	new file:   Assigned/Exercise 1-1/Solution/src/Start.java
    
    Description: first assigned exercise solved

---
+++ Assigned/Exercise 1-1/Solution/src/Start.java
@@ -0,0 +1,12 @@
+public class Start {
+    public static void main(String[] args) {
+        double base = 150.15d;
+        double altitude = 35.7d;
+        double area = (base * altitude) / 2;
+
+        System.out.println("The triangle has a base equal to " + base +
+                " and an altitude equal to " + altitude + " the resulting area" +
+                "is equal to " + area);
+
+    }
+}
