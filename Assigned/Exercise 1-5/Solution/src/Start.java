00efa5d5f2e6ade8bebf6b7156dd6db00bccb859
Fabio Frau <fbfr91@yahoo.it>
2023-09-26 11:27:05 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   Assigned/Exercise 1-5/Assignment.txt
    	new file:   Assigned/Exercise 1-5/Solution/.gitignore
    	new file:   Assigned/Exercise 1-5/Solution/.idea/.gitignore
    	new file:   Assigned/Exercise 1-5/Solution/.idea/misc.xml
    	new file:   Assigned/Exercise 1-5/Solution/.idea/modules.xml
    	new file:   Assigned/Exercise 1-5/Solution/.idea/vcs.xml
    	new file:   Assigned/Exercise 1-5/Solution/Solution.iml
    	new file:   Assigned/Exercise 1-5/Solution/src/Start.java
    
    Description: added the first exercise about methods

---
+++ Assigned/Exercise 1-5/Solution/src/Start.java
@@ -0,0 +1,14 @@
+public class Start {
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 10;
+
+        System.out.println(x + " + " + y + " = " + sumOf(x,y));
+
+    }
+
+    public static int sumOf (int x, int y) {
+        return x + y;
+    }
+}
