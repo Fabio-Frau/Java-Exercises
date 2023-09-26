16ec818a3999c2aca282a8cdb22b4e1b5b9722db
Fabio Frau <fbfr91@yahoo.it>
2023-09-26 10:44:36 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   Assigned/Exercise 1-3/Assignment.txt
    	new file:   Assigned/Exercise 1-3/Solution/.gitignore
    	new file:   Assigned/Exercise 1-3/Solution/.idea/.gitignore
    	new file:   Assigned/Exercise 1-3/Solution/.idea/misc.xml
    	new file:   Assigned/Exercise 1-3/Solution/.idea/modules.xml
    	new file:   Assigned/Exercise 1-3/Solution/.idea/vcs.xml
    	new file:   Assigned/Exercise 1-3/Solution/Solution.iml
    	new file:   Assigned/Exercise 1-3/Solution/src/Start.java
    
    Description: added the third exercise

---
+++ Assigned/Exercise 1-3/Solution/src/Start.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Start {
+    public static void main(String[] args) {
+        double x = 23.65;
+        double y = 36.7;
+
+        int result = (int)(x + y);
+
+        System.out.println(x  + " + " + y + " is equal to " + result);
+
+    }
+}
