0a43d854e2faea1f43ea5ad4fc7062864a381a6b
Fabio Frau <fbfr91@yahoo.it>
2023-09-27 09:50:01 +0200

    On branch development Your branch is up to date with 'origin/development'.
    Changes to be committed:
    	new file:   Assigned/Exercise 1-4/Assignment.txt
    	new file:   Assigned/Exercise 1-4/Solution/.gitignore
    	new file:   Assigned/Exercise 1-4/Solution/.idea/.gitignore
    	new file:   Assigned/Exercise 1-4/Solution/.idea/misc.xml
    	new file:   Assigned/Exercise 1-4/Solution/.idea/modules.xml
    	new file:   Assigned/Exercise 1-4/Solution/.idea/vcs.xml
    	new file:   Assigned/Exercise 1-4/Solution/Solution.iml
    	new file:   Assigned/Exercise 1-4/Solution/src/Start.java
    	modified:   Lessons/comparisonOperators/.idea/misc.xml
    
    Description: added second exercise about casting

---
+++ Assigned/Exercise 1-4/Solution/src/Start.java
@@ -0,0 +1,12 @@
+public class Start {
+    public static void main(String[] args) {
+
+        char a = 'a';
+        char Z = 'Z';
+
+        int result = a + Z;
+
+        System.out.println("The sum of the ASCII values of " + a + " and " + Z +
+                " is equal to " + result);
+    }
+}
