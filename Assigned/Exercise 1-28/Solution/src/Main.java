dc44a28eb2313788806fe8b00c9faf0217c8cb02
Fabio-Frau <fbfr91@yahoo.it>
2023-10-13 10:24:49 +0200

    Aggiornato esercizio 1-28

--- Assigned/Exercise 1-28/Solution/src/Main.java
+++ Assigned/Exercise 1-28/Solution/src/Main.java
@@ -2,13 +2,43 @@ public class Main {
     public static void main(String[] args) {
 
         double[] [] matrix = {{1,2,3}, {4,5,6}};
+        double[] [] matrix2 = {{1,2,3,4,5}, {7,8,9,10}};
 
-        System.out.println("Printing matrix");
+        System.out.println("Matrix:");
         printMatrix(matrix);
+        System.out.println("Matrix transposed");
+        transpose(matrix);
+        System.out.println("Matrix2:");
+        printMatrix(matrix2);
+        System.out.println("Matrix2 transposed:");
+        transpose(matrix2);
 
-        System.out.println("\nPrinting transposed matrix");
-        printMatrix(transposeMatrix(matrix));
+//        System.out.println("Printing matrix");
+//        printMatrix(matrix);
+//
+//        System.out.println("\nPrinting transposed matrix");
+//        printMatrix(transposeMatrix(matrix));
 
+    }
+
+    public static void transpose(double[][] matrix) {
+        if (isTransposable(matrix)) {
+            printMatrix(transposeMatrix(matrix));
+        } else {
+            System.out.println("The dimensions of the arrays inside the matrix don't allow to transpose the matrix");
+        }
+    }
+
+    public static boolean isTransposable(double[][] matrix) {
+
+        for (int i = 0 ; i < matrix.length; i++)
+        {
+            if (matrix[0].length != matrix[i].length) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static double [][] transposeMatrix(double[][] matrix) {
