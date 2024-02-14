78f971a3aefd09fac9a5774e728272bd92364ebb
Fabio-Frau <fbfr91@yahoo.it>
2024-02-14 19:31:04 +0100

    adding ex 10-8

---
+++ Assigned/Exercise 10-8/src/main/java/com/fabiofrau/Exercise8/Exercise108Application.java
@@ -0,0 +1,13 @@
+package com.fabiofrau.Exercise8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise108Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise108Application.class, args);
+	}
+
+}
---
+++ Assigned/Exercise 10-8/src/test/java/com/fabiofrau/Exercise8/Exercise108ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiofrau.Exercise8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise108ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
