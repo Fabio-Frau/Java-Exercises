4e66755d2778d8a5a7782e7ec6f919884f749898
Fabio-Frau <fbfr91@yahoo.it>
2024-02-14 18:44:11 +0100

    adding ex 10-6

---
+++ Assigned/Exercise 10-6/src/test/java/com/fabiofrau/Exercise6/Exercise106ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiofrau.Exercise6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise106ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
