3b86470f831531366fe589d66bcfc9ea9ecb063b
Fabio-Frau <fbfr91@yahoo.it>
2024-02-14 18:56:43 +0100

    adding ex 10-7

---
+++ Assigned/Exercise 10-7/src/main/java/com/fabiofrau/Exercise7/Exercise107Application.java
@@ -0,0 +1,13 @@
+package com.fabiofrau.Exercise7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise107Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise107Application.class, args);
+	}
+
+}
